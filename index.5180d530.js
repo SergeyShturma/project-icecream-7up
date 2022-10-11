184931d9554fc15b7ee05b8cfc5e4a6fb468d0c5
SergeyShturma <109282979+SergeyShturma@users.noreply.github.com>
2022-10-11 08:46:46 +0000

    Deploying to gh-pages from @ SergeyShturma/project-icecream-7up@f3168cf11339871b088e72f8283749b25538bef9 🚀

---
+++ index.5180d530.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".reviews__swiper",{pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensitivity:1,eventsTarget:".reviews__swiper"},loop:!0});
+//# sourceMappingURL=index.5180d530.js.map
